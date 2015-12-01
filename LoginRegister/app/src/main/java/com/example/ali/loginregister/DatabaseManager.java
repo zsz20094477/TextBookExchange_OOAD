7da0718a1662c95626b94999666701dd0cb0fda9
zsz20094477 <shuzhe.zhang@colorado.edu>
2015-12-01 15:01:18 -0700

    Each book function
    worked on search activity, seachManager and databaseManager

--- LoginRegister/app/src/main/java/com/example/ali/loginregister/DatabaseManager.java
+++ LoginRegister/app/src/main/java/com/example/ali/loginregister/DatabaseManager.java
@@ -31,8 +31,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
-
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhangshuzhe on 11/23/15.
@@ -46,6 +46,10 @@ public class DatabaseManager {
 
     private DatabaseManager() {
     }
+
+    //private Book[] book;
+    private Book[] book = new Book[10];
+    boolean search_done = false;
 
     public boolean addBook(String Owner_name, String Title, String Author, String Description, String Price, String Owner_Email, String Owner_Phone) {
         String url_select = "http://john-shuzhe.byethost13.com/PostBook.php";
@@ -166,12 +170,123 @@ public class DatabaseManager {
 
     }
 
-           public String searchBook(String searchBook)
+
+
+
+
+
+           public Book[] searchBook(String book_title)
            {
             //This fn. is receiving the book's name that user entered.
                // U need to search the book in the database and return the book's title and the price.
+               String url_select = "http://john-shuzhe.byethost13.com/SearchBook.php";
+               search_done = false;
 
-               return null;
+               //final URL url = new URL("http://john-shuzhe.byethost13.com/demo.php");
+               final HttpClient httpClient = new DefaultHttpClient();
+               final HttpPost httpPost = new HttpPost(url_select);
+
+               final ArrayList<NameValuePair> namePaires = new ArrayList<NameValuePair>();
+               namePaires.add(new BasicNameValuePair("Title", book_title));
+
+
+               class searchBook_task extends AsyncTask<String, String, Void>
+               {
+                   InputStream is = null ;
+                   String result = "";
+                   protected void onPreExecute() {
+
+                   }
+                   @Override
+                   protected Void doInBackground(String... params) {
+
+                       try {
+                           httpPost.setEntity(new UrlEncodedFormEntity(namePaires));
+
+                           HttpResponse httpResponse = httpClient.execute(httpPost);
+                           HttpEntity httpEntity = httpResponse.getEntity();
+                           System.out.println(httpEntity);
+
+                           //read content
+                           is =  httpEntity.getContent();
+
+
+                       } catch (Exception e) {
+
+                           Log.e("log_tag", "Error in http connection " + e.toString());
+                           //Toast.makeText(MainActivity.this, "Please Try Again", Toast.LENGTH_LONG).show();
+                       }
+
+                       try {
+                           BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                           StringBuilder sb = new StringBuilder();
+                           String line = "";
+                           while((line=br.readLine())!=null)
+                           {
+                               sb.append(line+"\n");
+                           }
+                           is.close();
+                           result=sb.toString();
+
+                       } catch (Exception e) {
+                           // TODO: handle exception
+                           Log.e("log_tag", "Error converting result "+e.toString());
+                       }
+
+                       try {
+                           System.out.println("Start: ");
+                           System.out.println(result);
+                           JSONArray Jarray = new JSONArray(result);
+
+                           for(int i=0;i<Jarray.length();i++) {
+                               JSONObject Jasonobject = null;
+                               //text_1 = (TextView)findViewById(R.id.txt1);
+                               Jasonobject = Jarray.getJSONObject(i);
+
+                               //get an output on the screen
+                               //String id = Jasonobject.getString("id");
+                               String Owner_name = Jasonobject.getString("Owner_name");
+                               String Title = Jasonobject.getString("Title");
+                               String Author = Jasonobject.getString("Author");
+                               String Description = Jasonobject.getString("Description");
+                               String Price = Jasonobject.getString("Price");
+                               String Owner_Email = Jasonobject.getString("Owner_Email");
+                               String Owner_Phone = Jasonobject.getString("Owner_Phone");
+                               String db_detail = "";
+                               System.out.println("haha2");
+                               book[i] = new Book(Owner_name, Title, Author, Description, Price, Owner_Email, Owner_Phone);
+                               System.out.println("haha3");
+                               search_done = true;
+                               System.out.println(book[i].getOwner_name());
+                           }
+
+                       } catch (Exception e) {
+                           // TODO: handle exception
+                           Log.e("log_tag", "Error parsing data "+e.toString());
+                       }
+                       return null;
+
+                   }
+                   protected void onPostExecute(Void v) {
+
+                       // ambil data dari Json database
+
+
+                   }
+               }
+               try {
+                   AsyncTask task = new searchBook_task().execute();
+                   //task.execute();
+                   task.get();
+                   System.out.println("haha4");
+               }
+               catch(Exception e){
+
+               }
+               return book;
+
+
+
            }
 
 public void deleteEntry(String book1)
